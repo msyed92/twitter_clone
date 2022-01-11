5a1b2b38123aa77aeb6a5f5659b3802a4e2adb69
Manaal <msyed@wpi.edu>
2022-01-10 22:40:36 -0500

    create signin/up and home pages

---
+++ client/src/routes/auth/login/index.svelte
@@ -0,0 +1,38 @@
+<script>
+	let loginMethods = ['username', 'email', 'phone number'];
+	$: method = loginMethods[0];
+	const changeMethod = (input) => {
+		method = input;
+	};
+
+	$: methodIndex = loginMethods.findIndex((e) => e === method);
+</script>
+
+<main>
+	<h1>Twitter 2.0 Sign In</h1>
+	<form action="/" method="GET">
+		<input type="text" placeholder={method} name={method} />
+
+		{#each loginMethods as method, i}
+			{#if i != methodIndex}
+				<small
+					on:click={() => {
+						changeMethod(method);
+					}}>Use {method} instead</small
+				>
+			{/if}
+		{/each}
+
+		<br />
+		<input type="password" placeholder="password" name="password" />
+		<br />
+		<button type="submit">Sign In</button>
+	</form>
+</main>
+
+<style>
+	main {
+		background-color: black;
+		color: white;
+	}
+</style>
---
+++ client/src/routes/auth/register/index.svelte
@@ -0,0 +1,23 @@
+<script>
+</script>
+
+<main>
+	<h1>Twitter 2.0 Sign Up</h1>
+	<form action="/" method="GET">
+		<input type="text" placeholder="username" name="username" />
+		<input type="password" placeholder="password" name="password" />
+		<input type="password" placeholder="confirm password" name="password-confirm" />
+		<input type="email" placeholder="email" name="email" />
+		<input type="text" placeholder="phone number (optional)" name="phone" />
+		<input type="text" placeholder="first name" name="firstName" />
+		<input type="text" placeholder="last name" name="lastName" />
+		<button type="submit">Sign Up</button>
+	</form>
+</main>
+
+<style>
+	main {
+		background-color: black;
+		color: white;
+	}
+</style>
