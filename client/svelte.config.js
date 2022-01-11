09954eaa226f80e359b9c74b6f39d3fcab684457
Manaal <msyed@wpi.edu>
2022-01-10 20:50:14 -0500

    add client side

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,17 +1,12 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			// default options are shown
-			out: 'build',
-			precompress: false,
-			env: {
-				host: 'HOST',
-				port: 'PORT'
-			}
-		})
+		adapter: adapter(),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
 	}
 };
 
